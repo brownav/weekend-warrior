0a9852090cb8aad68d4ada8642e8dec3f7e0aa69
Alexandria Brown <axbn2190@gmail.com>
2018-03-29 18:32:06 -0700

    removed check_length method and stored all login under class method compute

--- 03-hamming/hamming.rb
+++ 03-hamming/hamming.rb
@@ -1,18 +1,25 @@
 class Hamming
 
-  def initialize(strand1, strand2)
-    @strand1 = strand1
-    @strand2 = strand2
-  end
-
-  def check_length
+  def self.compute(strand1, strand2)
     if strand1.length != strand2.length
       raise ArgumentError.new("Incompatible lengths")
-    elsif strand1.length > 1 || strand2.length > 1
-      raise ArgumentError.new("Lengths must be greater than zero")
+    elsif strand1.length < 1 || strand2.length < 1
+      return 0
     end
-  end
 
-  
+    distance = 0
+    start = 0
+
+    while start < strand1.length
+      if strand1[start] == strand2[start]
+        start +=1
+      else
+        distance += 1
+        start += 1
+      end
+    end
+
+    return distance
+  end
 
 end
